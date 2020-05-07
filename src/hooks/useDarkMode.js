1b7cbab2e79cc7eddd89a160231509224f47ad08
Juan (programming) <programjuan@Juans-MacBook-Pro.local>
2020-05-07 14:36:51 -0400

    added the useEffect hook that is going to change the dom so when the darkmode value is true (this value is taken in by the key we pass in the function earlier) then it will set the darkmode class to the body and if its false then it will remove the class

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,7 +1,19 @@
 import useLocalStorage from './useLocalStorage';
+import { useEffect } from 'react';
 
 function useDarkMode(key){
+    //sets darkmode with the key
     const [darkMode, setDarkMode] = useLocalStorage(key);
-    
+    //useEffect hook that directly changes the DOM by...
+    useEffect(() =>{
+        //sets the darkmode class
+        if(darkMode === true){
+
+        }
+        //removes the darkmode class
+        else{
+
+        }
+    })
 
 }
